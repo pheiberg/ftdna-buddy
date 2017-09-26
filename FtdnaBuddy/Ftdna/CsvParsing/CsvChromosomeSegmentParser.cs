880252bd6ca7cda457a0f81d0c254311a293a761
Peter Heiberg <peter@heiberg.se>
2017-09-26 21:47:28 +0200

    Added custom csv parser for ChromosomeSegements

---
+++ FtdnaBuddy/Ftdna/CsvParsing/CsvChromosomeSegmentParser.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using FtdnaBuddy.Ftdna.Model;
+
+namespace FtdnaBuddy.Ftdna.CsvParsing
+{
+    public class CsvChromosomeSegmentParser
+    {
+        public IEnumerable<ChromosomeSegment> Parse(TextReader reader)
+        {
+            var segments = new List<ChromosomeSegment>();
+            
+            string line = reader.ReadLine();
+            if (line == null)
+                return segments;
+            
+            for(line = reader.ReadLine(); line != null; line = reader.ReadLine())
+            {
+                var processedLine = ParseLine(line);
+                if (processedLine != null)
+                {
+                    segments.Add(processedLine);
+                }
+            }
+            return segments;
+        }
+
+        private ChromosomeSegment ParseLine(string line)
+        {
+            string[] parts = line.Split(',');
+            
+            if (parts.Length < 7)
+                return null;
+
+            return new ChromosomeSegment
+            {
+                Name = parts[0].Trim('\"'),
+                MatchName = string.Join(',', parts.Skip(1).Take(parts.Length - 6)).Trim('\"'),
+                Chromosome = parts[parts.Length - 5],
+                StartLocation = long.Parse(parts[parts.Length - 4]),
+                EndLocation = long.Parse(parts[parts.Length - 3]),
+                Centimorgans = double.Parse(parts[parts.Length - 2]),
+                MatchingSnps = int.Parse(parts[parts.Length - 1])
+            };
+        }
+    }
+}
---
+++ UnitTests/CsvChromosomeSegmentParserTests.cs
@@ -0,0 +1,118 @@
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using FtdnaBuddy.Ftdna.CsvParsing;
+using FtdnaBuddy.Ftdna.Model;
+using Xunit;
+
+namespace UnitTests
+{
+    public class CsvChromosomeSegmentParserTests
+    {
+        [Fact]
+        public void SimpleLine_ShouldParseOk()
+        {
+            const string input = "NAME,MATCHNAME,CHROMOSOME,START LOCATION,END LOCATION,CENTIMORGANS,MATCHING SNPS\r\n" 
+                                 + "\"Peter Heiberg\",\"(MG) c/o Leanne Garlock\",1,2,3,4.5,6";
+            var reader = new StringReader(input);
+            var parser = new CsvChromosomeSegmentParser();
+
+            var result = parser.Parse(reader);
+
+            var expected = new ChromosomeSegment
+            {
+                Name = "Peter Heiberg",
+                MatchName = "(MG) c/o Leanne Garlock",
+                Chromosome = "1",
+                StartLocation = 2,
+                EndLocation = 3,
+                Centimorgans = 4.5,
+                MatchingSnps = 6
+            };
+            result.First().Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void MatchNameContainingUnescapedQuotes_ShouldParseOk()
+        {
+            const string input = "NAME,MATCHNAME,CHROMOSOME,START LOCATION,END LOCATION,CENTIMORGANS,MATCHING SNPS\r\n" 
+                                 + "\"John Doe\",\"Robert \"Bob\" Foo\",1,2,3,4.5,6";
+            var reader = new StringReader(input);
+            var parser = new CsvChromosomeSegmentParser();
+            
+            var result = parser.Parse(reader);
+
+            var expected = new ChromosomeSegment
+            {
+                Name = "John Doe",
+                MatchName = "Robert \"Bob\" Foo",
+                Chromosome = "1",
+                StartLocation = 2,
+                EndLocation = 3,
+                Centimorgans = 4.5,
+                MatchingSnps = 6
+            };
+            result.First().Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void RealFile_ShouldParseOk()
+        {
+            using(var file = File.OpenRead("../../../624718_Chromosome_Browser_Results_20170917.csv"))
+            using (var reader = new StreamReader(file))
+            {
+                var parser = new CsvChromosomeSegmentParser();
+
+                var result = parser.Parse(reader);
+
+                result.Should().HaveCountGreaterOrEqualTo(500);
+            }
+        }
+
+        [Fact]
+        public void MatchNameContainingCommas_ShouldParseOk()
+        {
+            const string input = "NAME,MATCHNAME,CHROMOSOME,START LOCATION,END LOCATION,CENTIMORGANS,MATCHING SNPS\r\n" 
+                               + "\"Peter Heiberg\",\"Erik Engebretsen,c/o Aage Harry Sørensen\",1,31908360,34303465,2.45,600";
+            var reader = new StringReader(input);
+            var parser = new CsvChromosomeSegmentParser();
+
+            var result = parser.Parse(reader);
+
+            var expected = new ChromosomeSegment
+            {
+                Name = "Peter Heiberg",
+                MatchName = "Erik Engebretsen,c/o Aage Harry Sørensen",
+                Chromosome = "1",
+                StartLocation = 31908360,
+                EndLocation = 34303465,
+                Centimorgans = 2.45,
+                MatchingSnps = 600
+            };
+            result.First().Should().BeEquivalentTo(expected);
+        }
+        
+        [Fact]
+        public void MatchNameContainingCommasAndQuotes_ShouldParseOk()
+        {
+            const string input = "NAME,MATCHNAME,CHROMOSOME,START LOCATION,END LOCATION,CENTIMORGANS,MATCHING SNPS\r\n" 
+                                 + "\"Peter Heiberg\",\"Robert \"Bob\", Doe\",1,31908360,34303465,2.45,600";
+            var reader = new StringReader(input);
+            var parser = new CsvChromosomeSegmentParser();
+
+            var result = parser.Parse(reader);
+
+            var expected = new ChromosomeSegment
+            {
+                Name = "Peter Heiberg",
+                MatchName = "Robert \"Bob\", Doe",
+                Chromosome = "1",
+                StartLocation = 31908360,
+                EndLocation = 34303465,
+                Centimorgans = 2.45,
+                MatchingSnps = 600
+            };
+            result.First().Should().BeEquivalentTo(expected);
+        }
+    }
+}
